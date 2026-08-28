8c0285afec9ab24f812f8f46c818591cc4b5924e
Charalampos Spanias <charalampos.spanias@csacyber.com>
2026-08-28 09:02:02 +0100

    Added testing account filtering.

--- src/ad_kit/core/util.py
+++ src/ad_kit/core/util.py
@@ -3,7 +3,7 @@ import typer
 
 from pathlib import Path
 
-from adk_kit.core.console import console, print_section
+from ad_kit.core.console import console, print_section
 
 ARTEFACTS_DIR = Path("ad-kit")
 
